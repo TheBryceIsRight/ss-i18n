56f1c315b9af2f1b48fdba39ba82a7ac20832e25
Bryce Watson <brycewatson315@gmail.com>
2020-07-21 14:19:58 -0400

    image resizing

--- components/Painting.tsx
+++ components/Painting.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import useTranslation from '../hooks/useTranslation'
 
+const imgStyles = {
+  width: '100%',
+};
+
 const Painting: React.FC = () => {
   const { t } = useTranslation()
   return (
     <div>
       <h1>{t('notAPipe')}</h1>
-      <img src="/img/not_a_pipe.jpg" alt={t('notAPipe')} />
+      <img src="/img/not_a_pipe.jpg" style={imgStyles} alt={t('notAPipe')}/>
     </div>
   )
 }
