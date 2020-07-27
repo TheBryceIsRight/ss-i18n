1bd659a1102971747a5b27d49f7346f9344c5878
Bryce Watson <brycewatson315@gmail.com>
2020-07-27 16:58:15 -0400

    skeleton broke something, reverting

--- components/Painting.tsx
+++ components/Painting.tsx
@@ -1,45 +1,18 @@
 import React from 'react'
 import useTranslation from '../hooks/useTranslation'
 import { Typography } from '@material-ui/core';
-import Skeleton from '@material-ui/lab/Skeleton';
-
-function TypographyDemo(props: { loading?: boolean }) {
-  const { loading = false } = props;
-  const { t } = useTranslation()
-  return (
-    <div>
-      <Typography component="div" variant='h2'>
-        {loading ? <Skeleton /> : t('notAPipe')}
-      </Typography>
-    </div>
-  );
-}
-
-function TypographyRectDemo(props: { loading?: boolean }) {
-  const { loading = false } = props;
-  const { t } = useTranslation()
-  return (
-    <div>
-      <Typography component="div" variant='h2'>
-        {loading ? <Skeleton  variant="rect" width={210} height={118} />
-         : <img src="/img/not_a_pipe.jpg" style={imgStyles} alt={t('notAPipe')}/>}
-      </Typography>
-    </div>
-  );
-}
 
 const imgStyles = {
   width: '100%',
 };
 
 const Painting: React.FC = () => {
-
+  const { t } = useTranslation()
   return (
     <div>
-      <TypographyDemo />
-      <br/>  
-      <TypographyRectDemo />
-
+      <Typography variant='h2'>{t('notAPipe')}</Typography>
+      <br/>
+      <img src="/img/not_a_pipe.jpg" style={imgStyles} alt={t('notAPipe')}/>
     </div>
   )
 }
