bbd3151a6f013f5df539754567a26f574c5775a8
Bryce Watson <brycewatson315@gmail.com>
2020-08-04 10:59:04 -0400

    made home page responsive

--- components/Artist.tsx
+++ components/Artist.tsx
@@ -2,15 +2,44 @@ import React from 'react'
 import useTranslation from '../hooks/useTranslation'
 import {Typography} from '@material-ui/core';
 import {Link} from '@material-ui/core';
+import { Media, MediaContextProvider } from "../utils/media";
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 
 const imgStyles = {
   width: '100%',
 };
 
+{/*GridList styles */}
+const useStyles1 = makeStyles((theme: Theme) =>
+createStyles({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 1100,
+    height: 600,
+  },
+  icon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+  },
+}),
+);
+
+
 const Artist: React.FC = () => {
-  const { locale, t } = useTranslation()
+  const { locale, t } = useTranslation();
+  const classes = useStyles1();
+
   return (
+    <MediaContextProvider>
+    <Media lessThan='lg'>
     <div>
       <Typography variant='h2'>René Magritte</Typography>
       <br/>
@@ -20,6 +49,22 @@ const Artist: React.FC = () => {
       <Typography variant='body1'>{t('bio')}</Typography>
       <Link href={`http://${locale}.wikipedia.org/wiki/René_Magritte`}>{t('readMore')}</Link>
     </div>
+    </Media>
+    <Media greaterThanOrEqual='lg'>
+    <Typography variant='h2'>René Magritte</Typography>
+    <br/>
+    <GridList cellHeight='auto' className={classes.gridList} spacing={10}>
+      <GridListTile>
+      <img src="/img/magritte.jpg" alt="Rene Magritte" style={imgStyles}/>
+      </GridListTile>
+      <GridListTile>
+        <Typography variant='body1'>{t('bio')}</Typography>
+        <br/>
+        <Link href={`http://${locale}.wikipedia.org/wiki/René_Magritte`}>{t('readMore')}</Link>
+      </GridListTile>
+    </GridList>
+    </Media>
+    </MediaContextProvider>
   )
 }
 
